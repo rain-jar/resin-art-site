bba158a6c85201ce8262a6d6186f9b4ab42450e4
Raj <raj.nair@colorado.edu>
2025-07-16 01:51:28 -0400

    header logo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,18 +9,25 @@ export default function Header() {
       <FaBars className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform" />
 
       {/* Center: Logo + Site Name */}
-      <div className="flex items-center gap-2">
+      <div className="flex items-center justify-center gap-0 relative">
+        {/* Logo */}
         <img 
           src="/images/logo.png" 
           alt="Logo" 
-          className="w-8 h-8 rounded-full"
+          className="w-10 h-10 md:w-12 md:h-14 rounded-full translate-x-[15px] md:translate-x-28"
         />
-      {/*  <span className="text-2xl font-bold">La Tête en Fleurs</span>*/}
-        <div className="h-8">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1146 188" preserveAspectRatio="xMidYMid meet" class="h-8">
-            <image href="/images/site-name.png" width="1146" height="188" />
+
+        {/* Site Name */}
+        <div className="flex items-center translate-x-[-10px] md:translate-x-20">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 1146 188" 
+            preserveAspectRatio="xMidYMid meet" 
+            className="h-8 md:h-14 lg:h-12"
+          >
+            <image href="/images/site-name-revised.png" width="1146" height="188" />
           </svg>
-       </div>
+        </div>
       </div>
 
       {/* Right: Menu Links + Icons */}
